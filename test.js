a87dc4b1949782ea0dde2f58f3120527201b2639
mauve <ranger@mauve.moe>
2020-10-02 13:18:31 -0400

    Log WebAssembly state to console

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ if (typeof Buffer === 'undefined') {
   global.Buffer = require('buffer').Buffer
 }
 
+console.log('# WebAssembly', global.WebAssembly)
+
 require('sodium-test')(require('.'))
 
 if ((typeof window !== 'undefined') && window.close) window.close()
