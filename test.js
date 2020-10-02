f9a78479cfeaa33248ee36ac45932a825064b0fa
mauve <ranger@mauve.moe>
2020-10-02 14:50:38 -0400

    Delete the WASM global if it's invalid

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
+// Lots of the sodium code assumes a global Buffer
+// If it isn't present, we should add it
 if (typeof Buffer === 'undefined') {
   global.Buffer = require('buffer').Buffer
 }
 
+// iOS defines a WebAssembly global, but doesn't provide a way to create instances
+// We shold delete the WebAssembly global in that case so that the tests pass
 if (typeof WebAssembly !== 'undefined' && global.WebAssembly) {
-  console.log('# WebAssembly', global.WebAssembly.Instance, global.WebAssembly.instantiate)
+  const canMakeInstance = global.WebAssembly.Instance || global.WebAssembly.instantiate
+  global.WebAssembly = undefined
 }
 
 require('sodium-test')(require('.'))
