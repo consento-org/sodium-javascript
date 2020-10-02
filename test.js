6322cb10a201caa26ab70bc53a567d4db6d074e1
mauve <ranger@mauve.moe>
2020-10-02 15:02:35 -0400

    Fix error deleting WASM on iOS

--- test.js
+++ test.js
@@ -8,7 +8,9 @@ if (typeof Buffer === 'undefined') {
 // We shold delete the WebAssembly global in that case so that the tests pass
 if (typeof WebAssembly !== 'undefined' && global.WebAssembly) {
   const canMakeInstance = global.WebAssembly.Instance || global.WebAssembly.instantiate
-  global.WebAssembly = undefined
+  if (canMakeInstance) {
+    global.WebAssembly = undefined
+  }
 }
 
 require('sodium-test')(require('.'))
